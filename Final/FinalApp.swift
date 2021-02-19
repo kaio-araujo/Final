70905dbdea76bb1965b03ce04f6ac70f8f23967a
Kaio <kaioaguanais@gmail.com>
2021-02-19 15:35:46 -0800

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by Kaio Guanais on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
